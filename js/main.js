c9c29979f8ef2039fa69bc58967a9bd7d2aefa2f
Liger76 <derskiy033@gmail.com>
2023-12-13 19:32:56 +0500

    JavaScript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var salom = alert('Assalomu alaykum')
+var isim = prompt('Ismingiz')
+console.log(isim);
+var tel = prompt("Telefon Nomeringizni kiriting")
+console.log(tel);
+var yosh = prompt('Yoshingizni kiriting')
+console.log(yosh);
