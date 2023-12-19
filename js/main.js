499887636c3bb5046444355a96262893fa9e9080
Liger76 <derskiy033@gmail.com>
2023-12-19 19:21:34 +0500

    js commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-var salom = alert('Assalomu alaykum')
-var isim = prompt('Ismingiz')
-console.log(isim);
-var tel = prompt("Telefon Nomeringizni kiriting")
-console.log(tel);
-var yosh = prompt('Yoshingizni kiriting')
-console.log(yosh);
+alert("Assalomu alaykum");
+var ism = prompt("Ismingizni kiriting")
+var yosh = prompt("Yoshingizni kiriting")
+if(!isNaN(yosh)){
+yosh = parseInt(yosh, 10);
+var elTitle = document.querySelector(".title")
+if(yosh > 18){
+elTitle.textContent = ism + ", oq yo'l!!!"
+}else if(yosh < 18){
+elTitle.textContent = ism + ", sen hali yoshsan!!!"
+}else{
+elTitle.textContent = ism + ", oq yo'l ehtiyot bo'l!!!"
+}
+}
